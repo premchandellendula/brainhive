94d8d31bbaf94bb2ac5b538acedfe6b99cf488ec
premchandellendula <premcellendula@gmail.com>
2025-05-10 21:04:20 +0530

    Add Backend_url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000/api/v1"
+export const BACKEND_URL = "https://brainhive-hrm4.onrender.com/api/v1"
+export const WEB_URL = "https://brainhive.vercel.app"
