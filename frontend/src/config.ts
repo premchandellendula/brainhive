785c982aa3aa4f06b614320930cd6a4c134489a3
premchandellendula <premcellendula@gmail.com>
2025-05-10 21:19:30 +0530

    add vercel.json and change web_url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 export const BACKEND_URL = "https://brainhive-hrm4.onrender.com/api/v1"
-export const WEB_URL = "https://brainhive-eight.vercel.app"
+export const WEB_URL = "https://brainhive-pc.vercel.app"
